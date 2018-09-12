1e803a728f8c91164eafcc7319966453a92343c9
Prateek Sahay <prateeksahay96@gmail.com>
2018-09-12 15:48:46 +0530

    hamming dist

--- Hamming/Hamming.cs
+++ Hamming/Hamming.cs
@@ -7,6 +7,25 @@ namespace Hamming
         public static int Distance(string original, string current)
         {
             int hammingDistance = 0;
+            if(original==null){
+                throw(new ArgumentNullException("original"));
+            }
+
+            if(current==null){
+                throw(new ArgumentNullException("current"));
+            }
+            
+            if(original.Length!=current.Length){
+                throw(new ArgumentException("Hamming Distance can only be calculated over strings of equal length"));
+            }
+
+            
+            
+            for(int i=0;i<original.Length;i++){
+                if(original[i]!=current[i]){
+                    hammingDistance++;
+                }
+            }
             
             return hammingDistance;
         }
